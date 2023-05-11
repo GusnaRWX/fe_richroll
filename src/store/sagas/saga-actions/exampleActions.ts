5e3dead68845d51d4c7b0424cdbd0297d46e9291
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-05-11 22:30:16 +0700

    fix: fixed build

--- src/store/sagas/saga-actions/exampleActions.ts
+++
@@ -1,9 +0,0 @@
-// import types from server type
-// import method from interceptors
-
-export function getExample(payload: string): Promise<string> {
-  // examplae
-  // const {page, limit, search, date} = payload
-  // return get(`/example?page=${page}&limit=${limit}&search=${search}&date=${date}`)
-  return new Promise((resolve) => resolve(payload));
-}
