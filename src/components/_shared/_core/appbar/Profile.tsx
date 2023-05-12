ef5566117b2642778276f6f6d1015b4e668b4440
Mochamad Akbar <mochakbar912@gmail.com>
2023-05-12 10:25:43 +0700

    feat(logout): add logout

--- src/components/_shared/_core/appbar/Profile.tsx
+++ src/components/_shared/_core/appbar/Profile.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import { Grid, Typography } from '@mui/material';
+import {Grid, Menu, MenuItem, Typography} from '@mui/material';
 import Image from 'next/image';
 import { Image as ImageType } from '@/utils/assetsConstant';
 import { IconButton } from '@/components/_shared/form';
 import ExpandLess from '@mui/icons-material/ExpandLess';
+import {useRouter} from 'next/router';
 
 const Profile = () => {
+  const router = useRouter();
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('user');
+    router.push('/login');
+    setAnchorElUser(null);
+  };
+
   return (
     <Grid
       container
@@ -43,7 +59,27 @@ const Profile = () => {
         </Grid>
       </Grid>
       <Grid item>
-        <IconButton icons={<ExpandLess />} />
+        <IconButton icons={<ExpandLess />} onClick={handleOpenUserMenu} />
+        <Menu
+          sx={{ mt: '45px' }}
+          id='menu-appbar'
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem onClick={handleCloseUserMenu}>
+            <Typography textAlign='center'>logout</Typography>
+          </MenuItem>
+        </Menu>
       </Grid>
     </Grid>
   );
