44cb9855a3c01474f3ebb8a54be704e0675e3148
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-05-13 20:54:45 +0700

    feat: add new layout profile

---
+++ src/containers/profile/ProfileContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@/components/_shared/_core/layout/Index';
+import ProfileComponent from '@/components/profile/ProfileComponent';
+
+const ProfileContainer = () => {
+  return (
+    <Layout>
+      <ProfileComponent />
+    </Layout>
+  );
+};
+
+export default ProfileContainer;
