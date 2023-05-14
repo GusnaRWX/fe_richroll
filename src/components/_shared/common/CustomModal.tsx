286a2aded26af54e6401152c6e6baa20432424b6
GusnaRWX <dadangusna777@gmail.com>
2023-05-14 19:56:32 +0700

    slicing create work schedule and add package schedule

---
+++ src/components/_shared/common/CustomModal.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Modal, Box, IconButton, Typography, Button as MuiButton } from '@mui/material';
+import { Close } from '@mui/icons-material';
+import styled from '@emotion/styled';
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  backgroundColor: '#FFFFFF',
+  border: '1px solid #E5E7EB',
+  borderRadius: '8px',
+  paddingTop: '10px',
+  p:2
+};
+
+const ModalHeader = styled.div`
+ display: flex;
+ flex-direction: row;
+ align-items: center;
+ justify-content: space-between;
+ margin: 0;
+ border-bottom: 1px solid #E5E7EB;
+ padding-bottom: .5rem;
+`;
+
+const ModalFooter = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: flex-end;
+margin: 0;
+gap: 1rem;
+border-top: 1px solid #E5E7EB;
+ padding-top: 1rem;
+`;
+
+
+interface CustomModalProps {
+  open: boolean;
+  handleClose: () => void;
+  title: string;
+  children;
+  width: string;
+}
+
+
+function CustomModal({open, handleClose, title, children, width}: CustomModalProps) {
+  const handleClick = () => {
+    handleClose();
+  };
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      keepMounted
+      disableAutoFocus
+    >
+      <Box sx={modalStyle} width={width}>
+        <ModalHeader>
+          <Typography component='h5' fontWeight='bold'>{title}</Typography>
+          <IconButton
+            onClick={handleClose}
+          ><Close /></IconButton>
+        </ModalHeader>
+        {children}
+        <ModalFooter>
+          <MuiButton variant='outlined' size='small' onClick={handleClose}>Cancel</MuiButton>
+          <MuiButton variant='contained' onClick={handleClick} size='small' color='primary'>Confirm</MuiButton>
+        </ModalFooter>
+      </Box>
+    </Modal>
+  );
+}
+
+export default CustomModal;
