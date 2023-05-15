0d0ddf2e41327e6f575b545538da0638d84fdbab
laukpindang <ferdiansyahbandung3@gmail.com>
2023-05-15 10:11:09 +0700

    .

--- src/components/company-profile/create/index.tsx
+++ src/components/company-profile/create/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/_shared/form";
+import AddIcon from "@mui/icons-material/Add";
+import { styled } from "@mui/material/styles";
 
 export default function CreateCompanyProfileComponent() {
+  const AddButton = styled(Button)({
+    color: "white",
+    maxWidth: "245px",
+    padding: "8px 10px",
+    ".MuiTypography-root": {
+      fontSize: "12px",
+    },
+  });
+
+  const supplementaryList = []
+
+  const addSuplementary = () => {
+    console.log('add'); 
+  }
+
   return (
-    <div>Index</div>
-  )
+    <div>
+      Form Here
+      <div>
+        <AddButton
+          color="secondary"
+          startIcon={<AddIcon />}
+          label="Add Supplementary Compensation"
+          onClick={() => addSuplementary()}
+        />
+      </div>
+    </div>
+  );
 }
