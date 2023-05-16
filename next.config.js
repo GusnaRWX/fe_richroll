bb6154994c91d90cd0da34df2e86f4d413cd76dd
Ramzi Tasha Mansoor <ramzitm91@gmail.com>
2023-05-17 00:37:58 +0700

    fix: update api company

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'ap-south-1.linodeobjects.com',
+        port: '',
+        pathname: '/sagara-staging/kayaroll-staging/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
