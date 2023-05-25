bfdf5f0dfa76287bbc616b0264afdc36ab79ec8f
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-05-25 16:27:01 +0700

    slicing index leave balance page

---
+++ src/pages/attendance-leave/balance/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LeaveBalanceContainer from "@/containers/attendance-leave/LeaveBalanceContainer";
+import MetaHead from "@/components/_shared/_core/layout/MetaHead";
+
+export default function Index() {
+  return (
+    <>
+      <MetaHead title="Kayaroll - Leave Balance" />
+      <LeaveBalanceContainer />
+    </>
+  );
+}
