c13f666d68e8743267dcdc3ccf13a22e56fd4ab6
GusnaRWX <dadangusna777@gmail.com>
2023-05-30 22:27:15 +0700

    fixing validation schema

--- src/components/auth/login/validate.ts
+++ src/components/auth/login/validate.ts
@@ -2,8 +2,8 @@ import * as Yup from 'yup';
 
 export const validationSchemeLogin = Yup.object({
   email: Yup.string().email('Email should be valid').required('Email is required'),
-  password: Yup.string().min(8, 'Password at least have 8 character long').matches(
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-    'Include Uppercase and lowercase, Include at least one number or symbol'
+  password: Yup.string().min(12, 'Password is too short(min of 12 character) and needs at least 1 upper case letter, 1 symbol and 1 number').matches(
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{12,}$/,
+    'Password is too short(min of 12 character) and needs at least 1 upper case letter, 1 symbol and 1 number'
   ).required('Password is required')
 });
