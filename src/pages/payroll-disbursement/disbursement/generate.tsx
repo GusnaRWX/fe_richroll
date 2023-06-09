44fc6366df4513e3a4d970919d6d75f38c6ee9f4
Ramzi Tasha Mansoor <ramzitm91@gmail.com>
2023-06-09 23:03:16 +0700

    feat: disbursement sub menu

---
+++ src/pages/payroll-disbursement/disbursement/generate.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DisbursementGenerateContainer from '@/containers/disbursement/DisbursementGenerateContainer';
+import MetaHead from '@/components/_shared/_core/layout/MetaHead';
+
+export default function DisbursementGenerate() {
+  return (
+    <>
+      <MetaHead title='Kayaroll - Disbursement Generate' />
+      <DisbursementGenerateContainer />
+    </>
+  );
+}
