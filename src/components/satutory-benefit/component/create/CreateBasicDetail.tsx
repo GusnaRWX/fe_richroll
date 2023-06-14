75b883c4e42b690f6d0e93a97ec85fbda17421d6
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-06-14 13:54:56 +0700

    .

--- src/components/satutory-benefit/component/create/CreateBasicDetail.tsx
+++ src/components/satutory-benefit/component/create/CreateBasicDetail.tsx
@@ -1,8 +1,26 @@
-/* eslint-disable @typescript-eslint/indent */
 import React from 'react';
+import { Grid } from '@mui/material';
+import { Button } from '@/components/_shared/form';
 
 export default function CreateBasicDetailComponent() {
     return(
-        <>1</>
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>p</Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
+                <Grid item xs={6} md={6} lg={6} xl={6}>
+                    <Button color='primary' label='Next' sx={{width:'63px'}}></Button>
+                </Grid>
+            </Grid>
+        </>
     )
 }
