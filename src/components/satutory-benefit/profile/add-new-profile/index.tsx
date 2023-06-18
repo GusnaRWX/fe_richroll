2ea7c61304aba00059a524cd5e5a58db8ba03b1e
M A DIMAS D P <madimas04@gmail.com>
2023-06-18 18:35:07 +0700

    add create profile component modal

--- src/components/satutory-benefit/profile/add-new-profile/index.tsx
+++ src/components/satutory-benefit/profile/add-new-profile/index.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Button,
   Input,
   Select,
   Textarea,
   IconButton,
-} from '@/components/_shared/form';
-import { styled } from '@mui/material/styles';
+  CheckBox,
+} from "@/components/_shared/form";
+import { styled } from "@mui/material/styles";
 import {
   Typography,
   MenuItem,
@@ -18,54 +19,65 @@ import {
   FormControl,
   Paper,
   Grid,
-} from '@mui/material';
-import { Download } from '@mui/icons-material';
-import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
-import BasicDatePicker from '@/components/_shared/form/DatePicker';
-import { ArrowBack } from '@mui/icons-material';
-import AddIcon from '@mui/icons-material/Add';
-import { useRouter } from 'next/router';
-import { Form as FormikForm, Formik, FieldArray } from 'formik';
-import * as Yup from 'yup';
-import dayjs from 'dayjs';
+} from "@mui/material";
+import { Download } from "@mui/icons-material";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import BasicDatePicker from "@/components/_shared/form/DatePicker";
+import { ArrowBack } from "@mui/icons-material";
+import AddIcon from "@mui/icons-material/Add";
+import { useRouter } from "next/router";
+import { Form as FormikForm, Formik, FieldArray } from "formik";
+import * as Yup from "yup";
+import dayjs from "dayjs";
+import { CustomModal } from "@/components/_shared/common";
 
 export default function CreateNewProfile() {
+  const [isAddNewComponent, setIsAddNewComponent] = useState(false);
+
+  function isCloseAddComponent() {
+    setIsAddNewComponent(false);
+  }
+
+  const handleConfirm = () => {
+    console.log("confirm");
+  };
+
   const router = useRouter();
 
-  const Header = styled('div')({
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: '32px',
+  const Header = styled("div")({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "32px",
   });
 
-  const HeaderPageTitle = styled('div')({
-    display: 'flex',
-    gap: '16px',
-    alignItems: 'center',
+  const HeaderPageTitle = styled("div")({
+    display: "flex",
+    gap: "16px",
+    alignItems: "center",
   });
 
   const NextBtnWrapper = styled(Box)({
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-end",
   });
 
   const AddButton = styled(Button)({
-    color: 'white',
-    maxWidth: '245px',
-    padding: '8px 10px',
-    '.MuiTypography-root': {
-      fontSize: '12px',
+    color: "white",
+    maxWidth: "245px",
+    padding: "8px 10px",
+    ".MuiTypography-root": {
+      fontSize: "14px",
     },
   });
 
   const Dummyoption = [
-    { value: '1', label: 'Dummy 1' },
-    { value: '2', label: 'Dummy 2' },
-    { value: '3', label: 'Dummy 3' },
+    { value: "1", label: "Dummy 1" },
+    { value: "2", label: "Dummy 2" },
+    { value: "3", label: "Dummy 3" },
   ];
 
   return (
@@ -73,18 +85,18 @@ export default function CreateNewProfile() {
       <Header>
         <HeaderPageTitle>
           <IconButton
-            parentColor='primary.500'
-            icons={<ArrowBack sx={{ color: '#FFFFFF' }} />}
+            parentColor="primary.500"
+            icons={<ArrowBack sx={{ color: "#FFFFFF" }} />}
             onClick={() => {
-              router.push('/attendance-leave/leave-summary');
+              router.push("/attendance-leave/leave-summary");
             }}
           />
           <Typography
             style={{
-              color: '#223567',
-              fontSize: '20px',
-              fontWeight: '700',
-              width: '250px',
+              color: "#223567",
+              fontSize: "20px",
+              fontWeight: "700",
+              width: "250px",
             }}
           >
             Leave Application
@@ -93,91 +105,91 @@ export default function CreateNewProfile() {
         <NextBtnWrapper>
           <Button
             fullWidth={false}
-            size='small'
-            label='Cancel'
-            variant='outlined'
-            sx={{ mr: '12px' }}
-            color='primary'
+            size="small"
+            label="Cancel"
+            variant="outlined"
+            sx={{ mr: "12px" }}
+            color="primary"
             onClick={() => {
-              router.push('/attendance-leave/leave-summary');
+              router.push("/attendance-leave/leave-summary");
             }}
           />
           <Button
             fullWidth={false}
-            size='small'
-            label='Save and Approve'
-            color='primary'
+            size="small"
+            label="Save and Approve"
+            color="primary"
             // onClick={() => formik.handleSubmit()}
           />
         </NextBtnWrapper>
       </Header>
-      <Paper style={{ padding: '21px 32px' }}>
-        <Paper style={{ padding: '21px 32px' }}>
+      <Paper style={{ padding: "21px 32px" }}>
+        <Paper style={{ padding: "21px 32px" }}>
           <Grid container spacing={2} rowSpacing={4}>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Input
-                placeholder='Input Statutory Benefits Name'
-                customLabel='Component Name'
+                placeholder="Input Statutory Benefits Name"
+                customLabel="Component Name"
                 withAsterisk
-                size='small'
+                size="small"
               />
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Select
-                placeholder='Select Country'
-                customLabel='Country'
+                placeholder="Select Country"
+                customLabel="Country"
                 withAsterisk
-                size='small'
+                size="small"
                 fullWidth
                 options={Dummyoption}
               />
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Select
-                placeholder='Select Country'
-                customLabel='Province'
-                size='small'
+                placeholder="Select Country"
+                customLabel="Province"
+                size="small"
                 fullWidth
                 options={Dummyoption}
               />
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Select
-                placeholder='Select Country'
-                customLabel='City'
-                size='small'
+                placeholder="Select Country"
+                customLabel="City"
+                size="small"
                 fullWidth
                 options={Dummyoption}
               />
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Select
-                placeholder='Select Country'
-                customLabel='Sub-District'
-                size='small'
+                placeholder="Select Country"
+                customLabel="Sub-District"
+                size="small"
                 fullWidth
                 options={Dummyoption}
               />
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
-              <BasicDatePicker customLabel='Effective Date' withAsterisk />
+              <BasicDatePicker customLabel="Effective Date" withAsterisk />
             </Grid>
             <Grid item xs={3} md={3} lg={3} xl={3}>
-              <BasicDatePicker customLabel='Expiration Date' />
+              <BasicDatePicker customLabel="Expiration Date" />
             </Grid>
             <Grid item xs={3} md={3} lg={3} xl={3}></Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Textarea
-                customLabel='Citation'
+                customLabel="Citation"
                 minRows={4}
-                style={{ resize: 'vertical' }}
+                style={{ resize: "vertical" }}
               />
               <Typography
                 style={{
-                  fontSize: '14px',
-                  marginTop: '6px',
-                  color: '#6B7280',
+                  fontSize: "14px",
+                  marginTop: "6px",
+                  color: "#6B7280",
                 }}
               >
                 Max. 120 Character
@@ -186,15 +198,15 @@ export default function CreateNewProfile() {
             <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Textarea
-                customLabel='Internal Notes'
+                customLabel="Internal Notes"
                 minRows={4}
-                style={{ resize: 'vertical' }}
+                style={{ resize: "vertical" }}
               />
               <Typography
                 style={{
-                  fontSize: '14px',
-                  marginTop: '6px',
-                  color: '#6B7280',
+                  fontSize: "14px",
+                  marginTop: "6px",
+                  color: "#6B7280",
                 }}
               >
                 Max. 120 Character
@@ -202,15 +214,15 @@ export default function CreateNewProfile() {
             </Grid>
             <Grid item xs={6} md={6} lg={6} xl={6}>
               <Textarea
-                customLabel='External Notes'
+                customLabel="External Notes"
                 minRows={4}
-                style={{ resize: 'vertical' }}
+                style={{ resize: "vertical" }}
               />
               <Typography
                 style={{
-                  fontSize: '14px',
-                  marginTop: '6px',
-                  color: '#6B7280',
+                  fontSize: "14px",
+                  marginTop: "6px",
+                  color: "#6B7280",
                 }}
               >
                 Max. 120 Character
@@ -218,13 +230,168 @@ export default function CreateNewProfile() {
             </Grid>
           </Grid>
         </Paper>
-        <Box style={{ padding: '21px 32px' }}>
+        <Box style={{ padding: "21px 32px" }}>
           <AddButton
-            color='secondary'
+            onClick={() => setIsAddNewComponent(true)}
+            color="secondary"
             startIcon={<AddIcon />}
-            label='Add Component'
+            label="Add Component"
+            sx={{ fontSize: "18px" }}
           />
         </Box>
+        <CustomModal
+          open={isAddNewComponent}
+          title="Select Satutory Benefits Components"
+          width="698px"
+          handleClose={isCloseAddComponent}
+          handleConfirm={handleConfirm}
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Employee Name" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Employee Name
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Effective Periode
+                </Typography>
+              </Grid>
+              <Grid item xs={2} md={2} lg={2} xl={2}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Rate Type
+                </Typography>
+              </Grid>
+              <Grid item xs={2} md={2} lg={2} xl={2}>
+                <AddButton
+                  variant="contained"
+                  startIcon={<AddIcon />}
+                  label="Component"
+                  sx={{ width: "136px", fontSize: "14px" }}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Health Insurance" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Health Insurance
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  09/05/2023
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Fixed Rate
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Pension" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Pension
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  09/05/2023
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Fixed Rate
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Death" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Death
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  09/05/2023
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Fixed Rate
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Old Age" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Old Age
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  -
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Fixed Amount
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container sx={{ display: "flex", alignItems: "center" }}>
+              <Grid item xs={1} md={1} lg={1} xl={1}>
+                <CheckBox name="Work Accident" customLabel="" />
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Work Accident
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  -
+                </Typography>
+              </Grid>
+              <Grid item xs={3} md={3} lg={3} xl={3}>
+                <Typography sx={{ fontWeight: 500, color: "#1F2937" }}>
+                  Matching
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
+        </CustomModal>
       </Paper>
     </>
   );
