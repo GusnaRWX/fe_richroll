f682929fd83a2227c53f51066db6a4f76b37bba5
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-06-19 12:07:33 +0700

    slicing satutory benefit profile detail page

---
+++ src/containers/satutory-benefit/SatutoryBenefitProfileDetailContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@/components/_shared/_core/layout/Index';
+import SatutoryBenefitProfileDetail from '@/components/satutory-benefit/profile/detail';
+
+const SatutoryBenefitProfileDetailContainer = () => {
+  return (
+    <Layout>
+      <SatutoryBenefitProfileDetail />
+    </Layout>
+  );
+};
+
+export default SatutoryBenefitProfileDetailContainer;
