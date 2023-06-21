ca5422128dc2391eab5d7503e5d4752df2411c5f
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-06-21 10:40:25 +0700

    slicing create basic detail tax profile

--- src/components/income-tax-profile/profile/add-new-profile/index.tsx
+++ src/components/income-tax-profile/profile/add-new-profile/index.tsx
@@ -7,9 +7,7 @@ import { ArrowBack } from '@mui/icons-material';
 import { useRouter } from 'next/router';
 
 // Import Create Component
-// import CreateBasicDetailComponent from './CreateBasicDetail';
-// import CreateDesignedTransferAccount from './CreateDesignedTransferAccount';
-// import CreateRates from './CreateRates';
+import CreateTaxBasicDetailComponent from './CreateTaxBasicDetail';
 
 export default function ItpAddNewProfileComponent() {
   const router = useRouter();
@@ -26,51 +24,6 @@ export default function ItpAddNewProfileComponent() {
     gap: '16px',
     alignItems: 'center',
   });
-
-  // const BpIcon = styled('span')(({ theme }) => ({
-  //   borderRadius: '50%',
-  //   width: 16,
-  //   height: 16,
-  //   boxShadow:
-  //     theme.palette.mode === 'dark'
-  //       ? '0 0 0 1px rgb(16 22 26 / 40%)'
-  //       : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-  //   backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
-  //   backgroundImage:
-  //     theme.palette.mode === 'dark'
-  //       ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
-  //       : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-  //   '.Mui-focusVisible &': {
-  //     outline: '2px auto rgba(19,124,189,.6)',
-  //     outlineOffset: 2,
-  //   },
-  //   'input:hover ~ &': {
-  //     backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
-  //   },
-  //   'input:disabled ~ &': {
-  //     boxShadow: 'none',
-  //     background:
-  //       theme.palette.mode === 'dark'
-  //         ? 'rgba(57,75,89,.5)'
-  //         : 'rgba(206,217,224,.5)',
-  //   },
-  // }));
-
-  // const BpCheckedIcon = styled(BpIcon)({
-  //   backgroundColor: '#137cbd',
-  //   backgroundImage:
-  //     'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-  //   '&:before': {
-  //     display: 'block',
-  //     width: 16,
-  //     height: 16,
-  //     backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
-  //     content: '',
-  //   },
-  //   'input:hover ~ &': {
-  //     backgroundColor: '#106ba3',
-  //   },
-  // });
 
   const [value, setValue] = useState(0);
 
@@ -129,18 +82,20 @@ export default function ItpAddNewProfileComponent() {
           </Typography>
         </HeaderPageTitle>
       </Header>
-      <Paper sx={{ width: '100%', p: '21px 32px' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Tabs value={value} onChange={handleChange} aria-label='basic tabs'>
-            <Tab label='Basic Detail' {...a11yProps(0)} />
-            <Tab label='Deductible Components' {...a11yProps(1)} />
-            <Tab label='Income Tax Rate' {...a11yProps(2)} />
-            <Tab label='Income Tax Multiplier' {...a11yProps(3)} />
-            <Tab label='Designated Transfer Account' {...a11yProps(4)} />
-          </Tabs>
+      <Paper sx={{ width: '100%', p: '21px 8px' }}>
+        <Box sx={{ px: 5 }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={value} onChange={handleChange} aria-label='basic tabs'>
+              <Tab label='Basic Detail' {...a11yProps(0)} />
+              <Tab label='Deductible Components' {...a11yProps(1)} />
+              <Tab label='Income Tax Rate' {...a11yProps(2)} />
+              <Tab label='Income Tax Multiplier' {...a11yProps(3)} />
+              <Tab label='Designated Transfer Account' {...a11yProps(4)} />
+            </Tabs>
+          </Box>
         </Box>
         <TabPanel value={value} index={0}>
-          {/* <CreateBasicDetailComponent setValue={setValue} /> */}
+          <CreateTaxBasicDetailComponent setValue={setValue} />
         </TabPanel>
         <TabPanel value={value} index={1}>
           {/* <CreateDesignedTransferAccount setValue={setValue} /> */}
