bb62e5d01b62db51aad6d2a921f0d7b671fef4dc
Your NameMoCheeseKy <rifkyking04@gmail.com>
2023-06-21 14:22:01 +0700

    slicing component detail for satutory page

--- src/components/satutory-benefit/profile/detail/index.tsx
+++ src/components/satutory-benefit/profile/detail/index.tsx
@@ -1,66 +1,47 @@
-import React, { useRef, useState } from "react";
+import React from 'react';
 import {
   Button,
-  Input,
-  Select,
-  Textarea,
   IconButton,
-  CheckBox,
-  Form,
-} from "@/components/_shared/form";
-import { styled } from "@mui/material/styles";
+} from '@/components/_shared/form';
+import { styled } from '@mui/material/styles';
 import {
   Typography,
-  MenuItem,
-  RadioGroup,
-  Radio,
-  FormControlLabel,
   Box,
-  FormHelperText,
-  FormControl,
   Paper,
   Grid,
-} from "@mui/material";
-import { Close, Download } from "@mui/icons-material";
-import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
-import BasicDatePicker from "@/components/_shared/form/DatePicker";
-import { ArrowBack } from "@mui/icons-material";
-import AddIcon from "@mui/icons-material/Add";
-import { useRouter } from "next/router";
-import { Form as FormikForm, Formik, FieldArray } from "formik";
-import * as Yup from "yup";
-import dayjs from "dayjs";
-import { CustomModal } from "@/components/_shared/common";
+} from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { useRouter } from 'next/router';
 
 // Icon Import
-import { HiPencilAlt } from "react-icons/hi";
-import { FiCopy } from "react-icons/fi";
-import { HiUpload } from "react-icons/hi";
-import { HiOutlineArchive } from "react-icons/hi";
+import { HiPencilAlt } from 'react-icons/hi';
+import { FiCopy } from 'react-icons/fi';
+import { HiUpload } from 'react-icons/hi';
+import { HiOutlineArchive } from 'react-icons/hi';
 
 export default function SatutoryBenefitProfileDetail() {
   const router = useRouter();
 
-  const Header = styled("div")({
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: "32px",
+  const Header = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: '32px',
   });
 
-  const HeaderPageTitle = styled("div")({
-    display: "flex",
-    gap: "16px",
-    alignItems: "center",
+  const HeaderPageTitle = styled('div')({
+    display: 'flex',
+    gap: '16px',
+    alignItems: 'center',
   });
 
   const NextBtnWrapper = styled(Box)({
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    gap: "12px",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    gap: '12px',
   });
 
   function EditActionHandler() {
@@ -72,18 +53,18 @@ export default function SatutoryBenefitProfileDetail() {
       <Header>
         <HeaderPageTitle>
           <IconButton
-            parentColor="primary.500"
-            icons={<ArrowBack sx={{ color: "#FFFFFF" }} />}
+            parentColor='primary.500'
+            icons={<ArrowBack sx={{ color: '#FFFFFF' }} />}
             onClick={() => {
-              router.push("/satutory-benefit/profile");
+              router.push('/satutory-benefit/profile');
             }}
           />
           <Typography
             style={{
-              color: "#223567",
-              fontSize: "20px",
-              fontWeight: "700",
-              width: "250px",
+              color: '#223567',
+              fontSize: '20px',
+              fontWeight: '700',
+              width: '250px',
             }}
           >
             Statutory Benefit Profile
@@ -91,111 +72,111 @@ export default function SatutoryBenefitProfileDetail() {
         </HeaderPageTitle>
         <NextBtnWrapper>
           <Button
-            color="inherit"
+            color='inherit'
             sx={{
-              bgcolor: "#fff",
-              color: "#223567",
-              width: "fit-content",
-              border: "1px solid #D1D5DB",
-              boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.05)",
+              bgcolor: '#fff',
+              color: '#223567',
+              width: 'fit-content',
+              border: '1px solid #D1D5DB',
+              boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.05)',
             }}
             startIcon={<FiCopy />}
-            label="Duplicate"
+            label='Duplicate'
           />
           <Button
-            color="orange"
-            sx={{ bgcolor: "#FFEDD5", color: "#F97316", width: "fit-content" }}
+            color='orange'
+            sx={{ bgcolor: '#FFEDD5', color: '#F97316', width: 'fit-content' }}
             startIcon={<HiOutlineArchive />}
-            label="Archive"
+            label='Archive'
           />
           <Button
-            color="green"
-            sx={{ bgcolor: "#8DD0B8", color: "#fff", width: "fit-content" }}
+            color='green'
+            sx={{ bgcolor: '#8DD0B8', color: '#fff', width: 'fit-content' }}
             startIcon={<HiPencilAlt />}
-            label="Edit"
+            label='Edit'
             onClick={EditActionHandler}
           />
           <Button
-            color="lightBlue"
-            sx={{ bgcolor: "#223567", color: "#fff", width: "fit-content" }}
+            color='lightBlue'
+            sx={{ bgcolor: '#223567', color: '#fff', width: 'fit-content' }}
             startIcon={<HiUpload />}
-            label="Activate"
+            label='Activate'
           />
         </NextBtnWrapper>
       </Header>
-      <Paper style={{ padding: "21px 32px" }}>
+      <Paper style={{ padding: '21px 32px' }}>
       <Grid container spacing={2}>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Profile Name</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Profile Name</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Country</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Country</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Province</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Province</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >City</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >City</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Sub-Distric</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Sub-Distric</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Effective Periode</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Effective Periode</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={6} md={6} lg={6} xl={6}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Citatation</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Citatation</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
           <Grid item xs={6} md={6} lg={6} xl={6}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >External Notes</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >External Notes</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={6} md={6} lg={6} xl={6}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Internal Notes</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Internal Notes</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
         </Grid>
       </Paper>
-      <Paper style={{ padding: "21px 32px", marginTop:'32px' }}>
+      <Paper style={{ padding: '21px 32px', marginTop:'32px' }}>
         <Grid container spacing={2}>
           <Grid item xs={12} md={12} lg={12} xl={12}>
           <Typography
             style={{
-              color: "#223567",
-              fontSize: "18px",
-              fontWeight: "700",
+              color: '#223567',
+              fontSize: '18px',
+              fontWeight: '700',
             }}
           >
             Old Age
           </Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Contributor</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Contributor</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Rate Types</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Rate Types</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={2} md={2} lg={2} xl={2}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Flat Rate </Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Flat Rate </Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={3} md={3} lg={3} xl={3}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Amount Cap</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Amount Cap</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
           <Grid item xs={3} md={3} lg={3} xl={3}>
-            <Typography component="div" variant="text-sm" color="#9CA3AF" mb="8px" >Effective Periode</Typography>
-            <Typography component="div" variant="text-sm" color="#4B5563">-</Typography>
+            <Typography component='div' variant='text-sm' color='#9CA3AF' mb='8px' >Effective Periode</Typography>
+            <Typography component='div' variant='text-sm' color='#4B5563'>-</Typography>
           </Grid>
         </Grid>
       </Paper>
