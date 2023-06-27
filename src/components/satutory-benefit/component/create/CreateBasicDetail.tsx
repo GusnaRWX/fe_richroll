ac4318785d6efde012849812c2800672dc6a771b
Dimas <madimas04@gmail.com>
2023-06-27 13:14:03 +0700

    feat/satutory-benefit--create: fix bug layouting

--- src/components/satutory-benefit/component/create/CreateBasicDetail.tsx
+++ src/components/satutory-benefit/component/create/CreateBasicDetail.tsx
@@ -1,14 +1,10 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { Grid, Box, Typography } from '@mui/material';
 import { Button, Input, Select, Textarea } from '@/components/_shared/form';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
-interface CreateBasicDetailComponentProps {
-  setValue: Dispatch<SetStateAction<number>>
-}
-
-export default function CreateBasicDetailComponent({setValue}: CreateBasicDetailComponentProps) {
+export default function CreateBasicDetailComponent({setValue}) {
   const Dummyoption = [
     { value: '1', label: 'Dummy 1' },
     { value: '2', label: 'Dummy 2' },
@@ -16,8 +12,8 @@ export default function CreateBasicDetailComponent({setValue}: CreateBasicDetail
   ];
 
   const validationSchema = Yup.object({
-    componentName: Yup.string().required(),
-    country: Yup.string().required(),
+    satutoryName: Yup.string().required('This field is Required!'),
+    country: Yup.string().required('This field is Required!'),
     province: Yup.string(),
     city: Yup.string(),
     subDistrict: Yup.string(),
@@ -28,7 +24,7 @@ export default function CreateBasicDetailComponent({setValue}: CreateBasicDetail
 
   const formik = useFormik({
     initialValues: {
-      componentName: '',
+      satutoryName: '',
       country: '',
       province: '',
       city: '',
@@ -46,129 +42,153 @@ export default function CreateBasicDetailComponent({setValue}: CreateBasicDetail
 
   return (
     <>
-      <Grid container spacing={2} rowSpacing={4} style={{paddingLeft:'-16px'}}>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Input
-            placeholder='Input Statutory Benefits Name'
-            customLabel='Component Name'
-            withAsterisk
-            size='small'
-            value={formik.values.componentName}
-            onChange={(e) => formik.setFieldValue('componentName', e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Select
-            placeholder='Select Country'
-            customLabel='Country'
-            withAsterisk
-            size='small'
-            fullWidth
-            options={Dummyoption}
-            value={formik.values.country}
-            onChange={(e) => formik.setFieldValue('country', e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6}>
-          <Select
-            placeholder='Select Province'
-            customLabel='Province'
-            size='small'
-            fullWidth
-            options={Dummyoption}
-            value={formik.values.province}
-            onChange={(e) => formik.setFieldValue('province', e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Select
-            placeholder='Select City'
-            customLabel='City'
-            size='small'
-            fullWidth
-            options={Dummyoption}
-            value={formik.values.city}
-            onChange={(e) => formik.setFieldValue('city', e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6}>
-          <Select
-            placeholder='Select Country'
-            customLabel='Sub-District'
-            size='small'
-            fullWidth
-            options={Dummyoption}
-            value={formik.values.subDistrict}
-            onChange={(e) => formik.setFieldValue('subDistrict', e.target.value)}
-          />
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Textarea
-            customLabel='Citation'
-            minRows={4}
-            style={{ resize: 'vertical' }}
-            value={formik.values.citation}
-            onChange={(e) => formik.setFieldValue('citation', e.target.value)}
-          />
-          <Typography
-            style={{
-              fontSize: '14px',
-              marginTop: '6px',
-              color: '#6B7280',
-            }}
-          >
-            Max. 120 Character
-          </Typography>
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6}></Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Textarea
-            customLabel='Internal Notes'
-            minRows={4}
-            style={{ resize: 'vertical' }}
-            value={formik.values.internalNotes}
-            onChange={(e) => formik.setFieldValue('internalNotes', e.target.value)}
-          />
-          <Typography
-            style={{
-              fontSize: '14px',
-              marginTop: '6px',
-              color: '#6B7280',
-            }}
-          >
-            Max. 120 Character
-          </Typography>
-        </Grid>
-        <Grid item xs={6} md={6} lg={6} xl={6} >
-          <Textarea
-            customLabel='External Notes'
-            minRows={4}
-            style={{ resize: 'vertical' }}
-            value={formik.values.externalNotes}
-            onChange={(e) => formik.setFieldValue('externalNotes', e.target.value)}
-          />
-          <Typography
-            style={{
-              fontSize: '14px',
-              marginTop: '6px',
-              color: '#6B7280',
-            }}
-          >
-            Max. 120 Character
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={12} lg={12} xl={12}>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button
-              color='primary'
-              label='Next'
-              sx={{ width: '63px' }}
-              onClick={() => formik.submitForm()}
+      <Box component='div'>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} lg={6} xl={6}>
+            <Input
+              placeholder='Input Statutory Benefits Name'
+              customLabel='Satutory Name'
+              withAsterisk
+              size='small'
+              value={formik.values.satutoryName}
+              onChange={(e) => formik.setFieldValue('satutoryName', e.target.value)}
             />
-          </Box>
+            {formik.touched.satutoryName && formik.errors.satutoryName ? (
+              <Typography sx={{color: '#DC2626',}}>{formik.errors.satutoryName}</Typography>
+            ): null}
+          </Grid>
         </Grid>
-      </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={6} md={6} lg={6} xl={6}>
+            <Select
+              placeholder='Select Country'
+              customLabel='Country'
+              withAsterisk
+              size='small'
+              fullWidth
+              options={Dummyoption}
+              value={formik.values.country}
+              onChange={(e) => formik.setFieldValue('country', e.target.value)}
+            />
+            {formik.touched.country && formik.errors.country ? (
+              <Typography sx={{color: '#DC2626'}}>{formik.errors.country}</Typography>
+            ): null}
+          </Grid>
+          <Grid item xs={6} md={6} lg={6} xl={6}>
+            <Select
+              placeholder='Select Province'
+              customLabel='Province'
+              size='small'
+              fullWidth
+              options={Dummyoption}
+              value={formik.values.province}
+              onChange={(e) => formik.setFieldValue('province', e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={6} md={6} lg={6} xl={6}>
+            <Select
+              placeholder='Select City'
+              customLabel='City'
+              size='small'
+              fullWidth
+              options={Dummyoption}
+              value={formik.values.city}
+              onChange={(e) => formik.setFieldValue('city', e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={6} md={6} lg={6} xl={6}>
+            <Select
+              placeholder='Select Country'
+              customLabel='Sub-District'
+              size='small'
+              fullWidth
+              options={Dummyoption}
+              value={formik.values.subDistrict}
+              onChange={(e) => formik.setFieldValue('subDistrict', e.target.value)}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={12} md={12} lg={12} xl={12}>
+            <Textarea
+              customLabel='Citation'
+              minRows={4}
+              style={{ resize: 'vertical' }}
+              value={formik.values.citation}
+              onChange={(e) => formik.setFieldValue('citation', e.target.value)}
+            />
+            <Typography
+              style={{
+                fontSize: '14px',
+                marginTop: '6px',
+                color: '#6B7280',
+              }}
+            >
+            Max. 120 Character
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={12} md={12} lg={12} xl={12}>
+            <Textarea
+              customLabel='Internal Notes'
+              minRows={4}
+              style={{ resize: 'vertical' }}
+              value={formik.values.internalNotes}
+              onChange={(e) => formik.setFieldValue('internalNotes', e.target.value)}
+            />
+            <Typography
+              style={{
+                fontSize: '14px',
+                marginTop: '6px',
+                color: '#6B7280',
+              }}
+            >
+            Max. 120 Character
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={12} md={12} lg={12} xl={12}>
+            <Textarea
+              customLabel='External Notes'
+              minRows={4}
+              style={{ resize: 'vertical' }}
+              value={formik.values.externalNotes}
+              onChange={(e) => formik.setFieldValue('externalNotes', e.target.value)}
+            />
+            <Typography
+              style={{
+                fontSize: '14px',
+                marginTop: '6px',
+                color: '#6B7280',
+              }}
+            >
+            Max. 120 Character
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} style={{marginTop: '12px'}}>
+          <Grid item xs={12} md={12} lg={12} xl={12}>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Button
+                color='primary'
+                label='Next'
+                sx={{ width: '63px' }}
+                onClick={() => formik.submitForm()}
+              />
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   );
 }
