3a340fe25a6396a59fa186ff2c789594d35d99aa
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-07-12 23:34:47 +0700

    fix: fixed design not same

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,12 +1,16 @@
 import MetaHead from '@/components/_shared/_core/layout/MetaHead';
 import ProfileContainer from '@/containers/profile/ProfileContainer';
 import React from 'react';
+import { useRouter } from 'next/router';
+import { useAppSelectors } from '@/hooks/index';
 
 const Profile = () => {
+  const router = useRouter();
+  const { profile } = useAppSelectors(state => state.me);
   return (
     <>
       <MetaHead title='Kayaroll - Profile' />
-      <ProfileContainer />
+      <ProfileContainer roles={router.query.role as string ?? profile?.roles?.join('')} />
     </>
   );
 };
