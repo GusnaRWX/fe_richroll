55c45834ca24955e1bb73fffaa33ae76af4f2b21
GusnaRWX <dadangusna777@gmail.com>
2023-07-24 22:09:16 +0700

    fix: fixing validation schema for create leave entries

--- src/components/leave-entries/validate.ts
+++ src/components/leave-entries/validate.ts
@@ -1,12 +1,13 @@
 import * as Yup from 'yup';
 
 export const validationSchemeCreateLeaveEntries = Yup.object({
-  leaveFrom: Yup.date().notRequired(),
-  leaveTo: Yup.date().when('leaveFrom', (leaveFrom, schema) => {
-    return schema.min(leaveFrom, 'Leave To harus lebih dari Leave From').notRequired();
-  }),
-  leaveType: Yup.string().notRequired(),
-  leaveStatus: Yup.string().notRequired(),
+  leaveFrom: Yup.date().required('This field is Required'),
+  leaveTo: Yup.date().required('This field is required'),
+  // leaveTo: Yup.date().when('leaveFrom', (leaveFrom, schema) => {
+  //   return schema.min(leaveFrom, 'Leave To harus lebih dari Leave From').notRequired();
+  // }),
+  leaveType: Yup.string().required('This field is required'),
+  leaveStatus: Yup.string().required('This field is required'),
   halfFrom: Yup.date().notRequired(),
   halfTo: Yup.date().notRequired(),
   note: Yup.string().notRequired()
