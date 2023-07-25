15c39e9911238ec97448a0ebfeba8fb47e200b07
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-07-25 21:46:10 +0700

    fix: adjust leave entries to be one date

--- src/components/leave-entries/validate.ts
+++ src/components/leave-entries/validate.ts
@@ -2,10 +2,6 @@ import * as Yup from 'yup';
 
 export const validationSchemeCreateLeaveEntries = Yup.object({
   leaveFrom: Yup.date().required('This field is Required'),
-  leaveTo: Yup.date().required('This field is required'),
-  // leaveTo: Yup.date().when('leaveFrom', (leaveFrom, schema) => {
-  //   return schema.min(leaveFrom, 'Leave To harus lebih dari Leave From').notRequired();
-  // }),
   leaveType: Yup.string().required('This field is required'),
   leaveStatus: Yup.string().required('This field is required'),
   halfFrom: Yup.date().notRequired(),
@@ -19,8 +15,5 @@ export const validationSchemeUpdateLeaveEntries = Yup.object({
   leaveFrom: Yup.date().notRequired(),
   halfTo: Yup.date().notRequired(),
   halfFrom: Yup.date().notRequired(),
-  leaveTo: Yup.date().min(
-    Yup.ref('leaveFrom'), 'Leave To harus lebih dari Leave From'
-  ),
   note: Yup.string()
 });
